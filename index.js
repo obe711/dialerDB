cfa1e1285188ca08ab670ed97ab256154c034c9c
obe711 <obe711@gmail.com>
2020-08-26 22:47:59 -0400

    add static dir

--- index.js
+++ index.js
@@ -3,21 +3,31 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const mongoose = require("mongoose");
-mongoose.connect(process.env.MONGO_DB, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-  useCreateIndex: true,
-});
 
 app.use(cors());
 app.use(express.json());
 
+// Mongoose Connection
+mongoose
+  .connect(process.env.MONGO_DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
+  .then(() => console.log(`Connected to ${process.env.NODE_ENV} MongoDB`))
+  .catch((err) => console.error("Could not connect...", err));
+
+// Static Routes
+app.use("/public", express.static("public"));
+
+// API Routes
 const tours = require("./routes/tours.js");
 app.use(`/api/${process.env.API_VERSION}/tours`, tours);
 
+// Start API Server
 app.listen(process.env.API_PORT, function () {
   console.log(
-    `SigConsulting Dialer API ${process.env.API_VERSION} on Port ${process.env.API_PORT}! - End Point: /api/${process.env.API_VERSION} (${process.env.NODE_ENV})`
+    `SigConsulting Dialer API ${process.env.API_VERSION} on Port ${process.env.API_PORT}! - End Point: /api/${process.env.API_VERSION}`
   );
 });
